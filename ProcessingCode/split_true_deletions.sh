a2e9e1dc82c651fc569f5d380ed8fb54dbbea6fc
timoast <timstuart90@gmail.com>
2016-06-07 13:06:10 +0800

    Add TE absence classifications

---
+++ ProcessingCode/split_true_deletions.sh
@@ -0,0 +1,13 @@
+# This script will split TE variants into two categories
+# 1. TE absences that are due to the lack of an insertion in the ancestral genome
+# 2. TE absences that are due to a true deletion of the ancestral TE
+# Some TE variants will have an NA call, where the classification is ambiguous
+# these will not be in either file
+
+gunzip -c ../RawData/TEPID_TEPAV.tsv.gz \
+| sed 1d \
+| awk 'BEGIN {FS=OFS="\t"} {if ($10 == "True deletion") print $0}' -> ../ProcessedData/TEPAV_true_deletions.tsv
+
+gunzip -c ../RawData/TEPID_TEPAV.tsv.gz \
+| sed 1d \
+| awk 'BEGIN {FS=OFS="\t"} {if ($10 == "No insertion") print $0}' - > ../ProcessedData/TEPAV_no_insertion.tsv
