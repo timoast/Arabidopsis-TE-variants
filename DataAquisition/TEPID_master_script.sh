1c3c0e3fddc96810720c9b134a703c7c366f97b6
timoast <timstuart90@gmail.com>
2016-06-07 10:51:08 +0800

    fix paths

--- DataAquisition/TEPID_master_script.sh
+++ DataAquisition/TEPID_master_script.sh
@@ -2,6 +2,8 @@
 # and identify TE insertion and TE absences with repect to the reference genome
 
 cd ../RawData
+# in RawData
+# Assuming source_PE_data.sh has been run and the PE reads are in folders in the RawData folder
 
 # download reference genome
 wget ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Arabidopsis_thaliana/NCBI/TAIR10/Arabidopsis_thaliana_NCBI_TAIR10.tar.gz
@@ -18,7 +20,7 @@ tepid-map -x tair10 -y ./TAIR10/Sequence/WholeGenomeFasta/genome.X15_01_65525S -
 discover () {
   for myfile in $(ls -d *.split.bam);do
     fname=(${myfile//.split.bam/ })
-    tepy-discover -n $fname -c "${fname}.bam" -s $myfile -t ../../../TAIR9_TE.bed.gz
+    tepy-discover -n $fname -c "${fname}.bam" -s $myfile -t ../TAIR9_TE.bed.gz
   done
 }
 
@@ -26,8 +28,10 @@ discover () {
 for directory in ./*; do
     if [ -d "$directory" ]; then
         cd $directory
+        # in RawData/Accession
         discover
         cd ..
+        # in RawData
     fi
 done
 
@@ -39,16 +43,19 @@ python create_poly_te.py
 # run refine step
 refine () {
   for myfile in $(ls -d *.split.bam);do
+    # in RawData/Accession
     fname=(${myfile//.split.bam/ })
-    tepy-refine -a ../../../accession_names.tsv -i ../../insertions.bed -d ../../deletions.bed -n $fname -c "${fname}.bam" -s $myfile -t ../../../TAIR9_TE.bed.gz
+    tepy-refine -a ../accession_names.tsv -i ../insertions.bed -d ../deletions.bed -n $fname -c "${fname}.bam" -s $myfile -t ../TAIR9_TE.bed.gz
   done
 }
 
 for directory in ./*; do
     if [ -d "$directory" ]; then
         cd $directory
-        map
+        # in RawData/Accession
+        refine
         cd ..
+        # in RawData
     fi
 done
 
