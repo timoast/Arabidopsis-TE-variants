30814eeef35b6708e62ae32ea18df92b53cb0e28
timoast <timstuart90@gmail.com>
2016-06-14 12:00:30 +0800

    Added volcano plots for each absence classification
    Adds to #2

--- AnalysisCode/gene_expression_volcano.R
+++ AnalysisCode/gene_expression_volcano.R
@@ -1,5 +1,5 @@
-library(dplyr)	
-library(ggplot2)	
+library(dplyr)
+library(ggplot2)
 library(qvalue)
 library(readr)
 
@@ -8,32 +8,32 @@ expression <- read_tsv("../RawData/gene_expression.tsv.gz", col_names = TRUE)
 upstream <- read_tsv("../ProcessedData/GeneFeatures/gene_upstream_regions_intersections.bed.gz", col_names = F) %>%
   select(X5:X12, X22) %>%
   mutate(gene = substr(X22, 4, 12)) %>%
-  rename(pos_accessions = X5, neg_accessions = X6)
+  rename(pos_accessions = X5, neg_accessions = X6, AbsenceClassification = X10)
 
 downstream <- read_tsv("../ProcessedData/GeneFeatures/gene_downstream_regions_intersections.bed.gz", col_names = F) %>%
   select(X5:X12, X22) %>%
   mutate(gene = substr(X22, 4, 12)) %>%
-  rename(pos_accessions = X5, neg_accessions = X6)
+  rename(pos_accessions = X5, neg_accessions = X6, AbsenceClassification = X10)
 
 exon <- read_tsv("../ProcessedData/GeneFeatures/exon_intersections.bed.gz", col_names = F) %>%
   mutate(gene = substr(X16, 8, 16)) %>%
   select(X5:X12, gene) %>%
-  rename(pos_accessions = X5, neg_accessions = X6)
+  rename(pos_accessions = X5, neg_accessions = X6, AbsenceClassification = X10)
 
 intron <- read_tsv("../ProcessedData/GeneFeatures/intron_intersections.bed.gz", col_names = F) %>%
   mutate(gene = substr(X16, 8, 16)) %>%
   select(X5:X12, gene) %>%
-  rename(pos_accessions = X5, neg_accessions = X6)
+  rename(pos_accessions = X5, neg_accessions = X6, AbsenceClassification = X10)
 
 utr5 <- read_tsv("../ProcessedData/GeneFeatures/utr5_intersections.bed.gz", col_names = F) %>%
   mutate(gene = substr(X16, 8, 16)) %>%
   select(X5:X12, gene) %>%
-  rename(pos_accessions = X5, neg_accessions = X6)
+  rename(pos_accessions = X5, neg_accessions = X6, AbsenceClassification = X10)
 
 utr3 <- read_tsv("../ProcessedData/GeneFeatures/utr3_intersections.bed.gz", col_names = F) %>%
   mutate(gene = substr(X16, 8, 16)) %>%
   select(X5:X12, gene) %>%
-  rename(pos_accessions = X5, neg_accessions = X6)
+  rename(pos_accessions = X5, neg_accessions = X6, AbsenceClassification = X10)
 
 upstream <- upstream[!duplicated(upstream),]	
 exon <- exon[!duplicated(exon),]	
@@ -69,8 +69,8 @@ utr5 %>%
 	
 downstream %>%	
   rowwise() %>%	
-  filter(find.len(pos_accessions) > 4) -> downstream.five	
- 	
+  filter(find.len(pos_accessions) > 4) -> downstream.five
+
 lookupExp <- function(accessions, gene, exp) {	
   x <- vector()	
   for(acc in accessions) {	
@@ -80,11 +80,12 @@ lookupExp <- function(accessions, gene, exp) {
 }	
 	
 test.sig.cor <- function(genes, expression){	
-  data <- matrix(nrow = nrow(genes), ncol = 4)	
+  data <- matrix(nrow = nrow(genes), ncol = 5)	
   # gene, te, acc names with, acc names without, p-val, cor	
   for(i in 1:nrow(genes)){	
     row <- genes[i,]
-    gene <- as.character(row[["gene"]])	
+    gene <- as.character(row[["gene"]])
+    absence <- row[["AbsenceClassification"]]
     ins.acc <- unlist(strsplit(gsub("-", "_", row[["pos_accessions"]]), ","))	
     no.ins.acc <- unlist(strsplit(gsub("-", "_", row[["neg_accessions"]]), ","))	
     ins.acc <- intersect(ins.acc, colnames(expression))	
@@ -100,18 +101,18 @@ test.sig.cor <- function(genes, expression){
             rep(2, length(no.ins.data))),	
           use="pairwise.complete.obs")	
       lfc <- log2(mean(ins.data)/mean(no.ins.data))	
-      data[i,] <- c(pval, r, lfc, gene)	
+      data[i,] <- c(pval, r, lfc, gene, absence)	
     }	
   }	
   return(data)	
 }	
 
-upstream.data <- test.sig.cor(upstream.five, expression)	
-exon.data <- test.sig.cor(exon.five, expression)	
-intron.data <- test.sig.cor(intron.five, expression)	
-utr3.data <- test.sig.cor(utr3.five, expression)	
-utr5.data <- test.sig.cor(utr5.five, expression)	
-downstream.data <- test.sig.cor(downstream.five, expression)	
+upstream.data <- test.sig.cor(upstream.five, expression)
+exon.data <- test.sig.cor(exon.five, expression)
+intron.data <- test.sig.cor(intron.five, expression)
+utr3.data <- test.sig.cor(utr3.five, expression)
+utr5.data <- test.sig.cor(utr5.five, expression)
+downstream.data <- test.sig.cor(downstream.five, expression)
 	
 # make one dataframe	
 exon.df <- as.data.frame(exon.data) %>% mutate(feature = "Exon", sample_size = nrow(exon.data))	
@@ -122,7 +123,7 @@ upstream.df <- as.data.frame(upstream.data) %>% mutate(feature = "Upstream", sam
 downstream.df <- as.data.frame(downstream.data) %>% mutate(feature = "Downstream", sample_size = nrow(downstream.data))	
 	
 all.data <- bind_rows(exon.df, intron.df, utr3.df, utr5.df, upstream.df, downstream.df)	
-colnames(all.data) <- c("pvalue", "cor", "log2FoldChange", "gene", "feature", "sample_size")	
+colnames(all.data) <- c("pvalue", "cor", "log2FoldChange", "gene", "AbsenceClassification", "feature", "sample_size")	
 all.data <- transform(all.data, pvalue = as.numeric(pvalue))	
 all.data <- transform(all.data, log2FoldChange = as.numeric(log2FoldChange))	
  	
@@ -151,7 +152,42 @@ ggplot(qval.data, aes(log2FoldChange, -log10(qvalue)), label = gene) +
   theme(legend.position="none") +	
   xlim(-6, 6)	+
   ggsave("../Plots/volcano_plots.pdf", width = 20, height = 4, units = "cm", useDingbats=F)	
-	
+
+ggplot(filter(qval.data, AbsenceClassification == "True deletion"), aes(log2FoldChange, -log10(qvalue)), label = gene) +	
+  geom_point(aes(color=sig, size=0.1)) + facet_wrap(~feature, nrow=1) + theme_bw(base_size = 8)+	
+  geom_hline(yintercept = -log10(sig_threshold), size = 0.1) +	
+  geom_vline(xintercept = -fc, size = 0.1) +	
+  geom_vline(xintercept = fc, size = 0.1) +	
+  scale_size_identity() +	
+  scale_color_manual(values = c("blue", "grey", "red")) +	
+  theme(legend.position="none") +	
+  xlim(-6, 6) +
+  ggsave("../Plots/volcano_plots_deletions.pdf", width = 20, height = 4, units = "cm", useDingbats=F)	
+
+
+ggplot(filter(qval.data, AbsenceClassification == "No insertion"), aes(log2FoldChange, -log10(qvalue)), label = gene) +	
+  geom_point(aes(color=sig, size=0.1)) + facet_wrap(~feature, nrow=1) + theme_bw(base_size = 8)+	
+  geom_hline(yintercept = -log10(sig_threshold), size = 0.1) +	
+  geom_vline(xintercept = -fc, size = 0.1) +	
+  geom_vline(xintercept = fc, size = 0.1) +	
+  scale_size_identity() +	
+  scale_color_manual(values = c("blue", "grey", "red")) +	
+  theme(legend.position="none") +	
+  xlim(-6, 6) +
+  ggsave("../Plots/volcano_plots_insertions.pdf", width = 20, height = 4, units = "cm", useDingbats=F)	
+
+
+ggplot(filter(qval.data, is.na(AbsenceClassification)), aes(log2FoldChange, -log10(qvalue)), label = gene) +	
+  geom_point(aes(color=sig, size=0.1)) + facet_wrap(~feature, nrow=1) + theme_bw(base_size = 8)+	
+  geom_hline(yintercept = -log10(sig_threshold), size = 0.1) +	
+  geom_vline(xintercept = -fc, size = 0.1) +	
+  geom_vline(xintercept = fc, size = 0.1) +	
+  scale_size_identity() +	
+  scale_color_manual(values = c("blue", "grey", "red")) +	
+  theme(legend.position="none") +	
+  xlim(-6, 6) +
+  ggsave("../Plots/volcano_plots_na_absence_call.pdf", width = 20, height = 4, units = "cm", useDingbats=F)	
+
 qval.data %>%
   filter(sig != "NS") %>%
   write.table(., file="../ProcessedData/sig_genes.tsv", quote = F, sep = "\t", row.names = F)
