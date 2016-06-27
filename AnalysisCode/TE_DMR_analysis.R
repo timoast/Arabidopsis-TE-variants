c065638af1221e378c6694fae99594e05a309b76
timoast <timstuart90@gmail.com>
2016-06-27 20:44:49 +0800

    Run bootstraping on insertions & deletions separately
    Generate new bootstrapping data and plots,
    relates to #4
    
    :   AnalysisCode/TE_DMR_analysis.R

--- AnalysisCode/TE_DMR_analysis.R
+++ AnalysisCode/TE_DMR_analysis.R
@@ -22,6 +22,9 @@ cg <- "#B4B464"
 chg <- "#6665AD"
 chh <- "#B29492"
 
+insertion_col <- brewer.pal(3, "Set2")[1]
+deletion_col <- brewer.pal(3, "Set2")[3]
+
 pdf("../Plots/distances_TEPAV_DMR.pdf", height = 3, width = 3)
 hist(te_c_dmr$distance / 1000,
      breaks = 200, xlim = c(0, 20),
@@ -31,6 +34,9 @@ hist(te_cg_dmr$distance / 1000,
      col = cg, xlab = "Distance (kb)", main = "Distance from TE variant to nearest CG-DMR")
 dev.off()
 
+te_c_dmr$AbsenceClassification <- factor(te_c_dmr$AbsenceClassification, levels = c("True deletion", "No insertion"))	
+te_cg_dmr$AbsenceClassification <- factor(te_cg_dmr$AbsenceClassification, levels = c("True deletion", "No insertion"))	
+
 # C-DMRs
 ggplot(te_c_dmr, aes(distance / 1000, fill = FrequencyClassification)) +
   geom_density(alpha = 0.5) + facet_wrap(~FrequencyClassification) + xlim(0, 5) +
@@ -38,11 +44,12 @@ ggplot(te_c_dmr, aes(distance / 1000, fill = FrequencyClassification)) +
   theme(legend.position="none") +	
   ggsave("../Plots/CDMR/c_dmr_distances_common_vs_rare.pdf", width = 10, height = 6, units = "cm", useDingbats=F)	
 
-ggplot(te_c_dmr, aes(distance / 1000, fill = AbsenceClassification)) +
-  geom_density(alpha = 0.5) + facet_wrap(~AbsenceClassification) + xlim(0, 5) +
+ggplot(na.omit(te_c_dmr), aes(distance / 1000, fill = AbsenceClassification)) +
+  geom_density(alpha = 0.8) + facet_wrap(~AbsenceClassification) + xlim(0, 5) +
   theme_bw() + xlab("Distance (kb)") + ylab("Density") +
   theme(legend.position="none") +	
-  ggsave("../Plots/CDMR/c_dmr_distances_insertion_vs_deletion.pdf", width = 15, height = 6, units = "cm", useDingbats=F)
+  scale_fill_manual(values =c(deletion_col, insertion_col)) + 
+  ggsave("../Plots/CDMR/c_dmr_distances_insertion_vs_deletion.pdf", width = 10, height = 6, units = "cm", useDingbats=F)
 
 # CG-DMRs
 ggplot(te_cg_dmr, aes(distance / 1000, fill = FrequencyClassification)) +
@@ -51,30 +58,58 @@ ggplot(te_cg_dmr, aes(distance / 1000, fill = FrequencyClassification)) +
   theme(legend.position="none") +	
   ggsave("../Plots/CGDMR/cg_dmr_distances_common_vs_rare.pdf", width = 10, height = 6, units = "cm", useDingbats=F)	
 
-ggplot(te_cg_dmr, aes(distance / 1000, fill = AbsenceClassification)) +
-  geom_density(alpha = 0.5) + facet_wrap(~AbsenceClassification) + xlim(0, 5) +
+ggplot(na.omit(te_cg_dmr), aes(distance / 1000, fill = AbsenceClassification)) +
+  geom_density(alpha = 0.8) + facet_wrap(~AbsenceClassification) + xlim(0, 5) +
   theme_bw() + xlab("Distance (kb)") + ylab("Density") +
   theme(legend.position="none") +	
-  ggsave("../Plots/CGDMR/cg_dmr_distances_insertion_vs_deletion.pdf", width = 15, height = 6, units = "cm", useDingbats=F)
+  scale_fill_manual(values =c(deletion_col, insertion_col)) + 
+  ggsave("../Plots/CGDMR/cg_dmr_distances_insertion_vs_deletion.pdf", width = 10, height = 6, units = "cm", useDingbats=F)
 
 # percentage of DMRs with TE variant within 1 kb, compared to random regions replicated 10k times
 # see ProcessingCode/bootstrap_te_variants.sh for source of random overlap percentages
 c_dmrs <- read_tsv("../RawData/c_dmrs.tsv.gz", col_names = F)
 cg_dmrs <- read_tsv("../RawData/cg_dmrs.tsv.gz", col_names = F)
 
-bootstrap_c_dmr_1kb <- read_table("../ProcessedData/random_selections_c_dmr_rep_10k.txt", col_names = F)
-bootstrap_cg_dmr_1kb <- read_table("../ProcessedData/random_selections_cg_dmr_rep_10k.txt", col_names = F)
+bootstrap_c_dmr_insertions_1kb <- read_table("../ProcessedData/random_selections_c_dmr_rep_10k_insertions.txt", col_names = F)
+bootstrap_c_dmr_deletions_1kb <- read_table("../ProcessedData/random_selections_c_dmr_rep_10k_deletions.txt", col_names = F)
 
-pdf("../Plots/DMR_intersections_bootstrap.pdf", height = 3, width = 3)
-hist(bootstrap_c_dmr_1kb$X1/nrow(c_dmrs) * 100,
-     breaks = 10, xlim = c(0, 100), xlab = "Percent within 1 kb",
-     main = "TE variants near C-DMRs", border = chh)
-abline(v=nrow(te_c_dmr %>% filter(distance < 1000)) / nrow(c_dmrs) * 100)
+bootstrap_cg_dmr_insertions_1kb <- read_table("../ProcessedData/random_selections_cg_dmr_rep_10k_insertions.txt", col_names = F)
+bootstrap_cg_dmr_deletions_1kb <- read_table("../ProcessedData/random_selections_cg_dmr_rep_10k_deletions.txt", col_names = F)
 
-hist(bootstrap_cg_dmr_1kb$X1/nrow(cg_dmrs) * 100,
+counts <- read_tsv("../RawData/TEPID_TEPAV.tsv.gz", col_names = T) %>%
+  mutate(l = end-start) %>%
+  group_by(Absence_classification) %>%
+  summarise(ml = mean(l), c = n())
+
+pdf("../Plots/DMR_intersections_bootstrap.pdf", height = 5, width = 5)
+par(mfrow = (c(2,2)))
+hist(bootstrap_c_dmr_deletions_1kb$X1/counts$c[2] * 100,
      breaks = 10, xlim = c(0, 100), xlab = "Percent within 1 kb",
-     main = "TE variants near CG-DMRs", border = cg)
-abline(v=nrow(te_cg_dmr %>% filter(distance < 1000)) / nrow(cg_dmrs) * 100)
+     main = "TE deletions near C-DMRs", border = chh)
+intersections <- nrow(filter(te_c_dmr, AbsenceClassification == "True deletion" & distance < 1000))
+total_class <- nrow(filter(te_c_dmr, AbsenceClassification == "True deletion"))
+abline(v=intersections / total_class * 100)
+
+hist(bootstrap_c_dmr_insertions_1kb$X1/counts$c[1] * 100,
+     breaks = 10, xlim = c(0, 100), xlab = "Percent within 1 kb",
+     main = "TE insertions near C-DMRs", border = chh)
+intersections <- nrow(filter(te_c_dmr, AbsenceClassification == "No insertion" & distance < 1000))
+total_class <- nrow(filter(te_c_dmr, AbsenceClassification == "No insertion"))
+abline(v=intersections / total_class * 100)
+
+hist(bootstrap_cg_dmr_deletions_1kb$X1/counts$c[2] * 100,
+     breaks = 10, xlim = c(0, 100), xlab = "Percent within 1 kb",
+     main = "TE deletions near CG-DMRs", border = cg)
+intersections <- nrow(filter(te_cg_dmr, AbsenceClassification == "True deletion" & distance < 1000))
+total_class <- nrow(filter(te_cg_dmr, AbsenceClassification == "True deletion"))
+abline(v=intersections / total_class * 100)
+
+hist(bootstrap_cg_dmr_insertions_1kb$X1/counts$c[1] * 100,
+     breaks = 10, xlim = c(0, 100), xlab = "Percent within 1 kb",
+     main = "TE insertions near CG-DMRs", border = cg)
+intersections <- nrow(filter(te_cg_dmr, AbsenceClassification == "No insertion" & distance < 1000))
+total_class <- nrow(filter(te_cg_dmr, AbsenceClassification == "No insertion"))
+abline(v=intersections / total_class * 100)
 dev.off()
 
 # TE-DMR methylation level
@@ -128,9 +163,9 @@ te_cg_dmr <- te_cg_dmr %>%
 
 ### C-DMRs ###
 # Make dataframe
-c_dmr_info <- na.omit(te_c_dmr %>%
-  mutate(class = ifelse(distance < 1000, "TE-DMR", "Non-TE-DMR")) %>%
-  select(AbsenceClassification, class, mC_pos, mC_neg, r2, distance)) %>%
+c_dmr_info <- te_c_dmr %>%
+  mutate(class = ifelse(distance < 1000, "TE-DMR", "Non-TE-DMR")) %>% 
+  select(AbsenceClassification, class, mC_pos, mC_neg, r2, distance) %>% 
   gather(TE_present, mC, mC_pos:mC_neg)
 
 c_dmr_info$TE_present[c_dmr_info$TE_present == "mC_pos"] <- TRUE
@@ -148,10 +183,11 @@ ggplot(c_dmr_info, aes(r2, distance/1000)) +
 
 # on log scale with linear regression
 ggplot(c_dmr_info, aes(r2, distance)) +
-  geom_point(alpha=0.1) + theme_bw() + facet_wrap(~AbsenceClassification) +
+  geom_point(alpha=0.1, size=0.4) + theme_bw() + facet_wrap(~AbsenceClassification) +
   scale_y_log10() + geom_smooth(method = "lm") +
+  xlim(-1,1) +
   ylab("Distance to C-DMR") + xlab("r2") +
-  ggsave("../Plots/CDMR/c_dmr_distance_vs_r2_log.pdf", height=4, width = 6, useDingbats=F)
+  ggsave("../Plots/CDMR/c_dmr_distance_vs_r2_log.png", height=3, width = 5)
 
 # Distribution of r2 values for TE-DMRs vs non-TE-DMRs, for insertions and deletions
 ggplot(c_dmr_info, aes(r2, col=class)) +
@@ -178,9 +214,9 @@ ggplot(c_dmr_info, aes(TE_present, mC)) + theme_bw() +
 
 ### CG-DMRs ###
 # Make dataframe
-cg_dmr_info <- na.omit(te_cg_dmr %>%
+cg_dmr_info <- te_cg_dmr %>%
                         mutate(class = ifelse(distance < 1000, "TE-DMR", "Non-TE-DMR")) %>%
-                        select(AbsenceClassification, class, mC_pos, mC_neg, r2, distance)) %>%
+                        select(AbsenceClassification, class, mC_pos, mC_neg, r2, distance) %>%
   gather(TE_present, mC, mC_pos:mC_neg)
 
 cg_dmr_info$TE_present[cg_dmr_info$TE_present == "mC_pos"] <- TRUE
@@ -194,14 +230,15 @@ cg_dmr_info$AbsenceClassification[cg_dmr_info$AbsenceClassification == "True del
 ggplot(cg_dmr_info, aes(r2, distance/1000)) +
   geom_point(alpha=0.1) + theme_bw() + facet_wrap(~AbsenceClassification) +
   ylab("Distance to CG-DMR (kb)") + xlab("r2") +
-  ggsave("../Plots/CGDMR/cg_dmr_distance_vs_r2.pdf", height=4, width = 6, useDingbats=F)
+  ggsave("../Plots/CGDMR/cg_dmr_distance_vs_r2.pdf", height=3, width = 5, useDingbats=F)
 
 # on log scale with linear regression
 ggplot(cg_dmr_info, aes(r2, distance)) +
-  geom_point(alpha=0.1) + theme_bw() + facet_wrap(~AbsenceClassification) +
+  geom_point(alpha=0.1, size=0.4) + theme_bw() + facet_wrap(~AbsenceClassification) +
   scale_y_log10() + geom_smooth(method = "lm") +
+  xlim(-1,1) +
   ylab("Distance to CG-DMR") + xlab("r2") +
-  ggsave("../Plots/CGDMR/cg_dmr_distance_vs_r2_log.pdf", height=4, width = 6, useDingbats=F)
+  ggsave("../Plots/CGDMR/cg_dmr_distance_vs_r2_log.png", height=3, width = 5)
 
 # Distribution of r2 values for TE-DMRs vs non-TE-DMRs, for insertions and deletions
 ggplot(cg_dmr_info, aes(r2, col=class)) +
--- ProcessingCode/bootstrap_te_variants.sh
+++ ProcessingCode/bootstrap_te_variants.sh
@@ -1,10 +1,28 @@
-tepav=23095
-av_size=501
+#! /bin/sh
 
+# to get mean length and number of each TE variant type (in R):
+# library(readr)
+# library(dplyr)
+# 
+# read_tsv("../RawData/TEPID_TEPAV.tsv.gz", col_names = T) %>%
+#   mutate(l = end-start) %>%
+#   group_by(Absence_classification) %>%
+#   summarise(ml = mean(l), c = n())
+
+all=23095
+insertions=15077
+deletions=5856
+inter=2162
+
+all_size=501
+ins_size=134
+del_size=1170
+inter_size=1249
 
 for (( i=1; i <= 10000; i++ )); do
-     # choose random coordinates
-    python choose_random_coords.py $tepav $av_size \
+    ### All
+    # choose random coordinates
+    python choose_random_coords.py $all $all_size \
     | sort -k1,1 -k2,2n - \
     > ../ProcessedData/random_coords.bed
 
@@ -19,6 +37,60 @@ for (( i=1; i <= 10000; i++ )); do
     | awk 'BEGIN {FS=OFS="\t"} {if ($7 < 1000) print $0}' - \
     | wc -l \
     >> ../ProcessedData/random_selections_cg_dmr_rep_10k.txt
+
+    ### insertions
+    # choose random coordinates
+    python choose_random_coords.py $insertions $ins_size \
+    | sort -k1,1 -k2,2n - \
+    > ../ProcessedData/random_coords_insertions.bed
+
+    # c-dmrs
+    bedtools closest -a ../ProcessedData/random_coords_insertions.bed -b ../RawData/c_dmrs.tsv.gz -d -wb \
+    | awk 'BEGIN {FS=OFS="\t"} {if ($7 < 1000) print $0}' - \
+    | wc -l \
+    >> ../ProcessedData/random_selections_c_dmr_rep_10k_insertions.txt
+
+    #cg-dmrs
+    bedtools closest -a ../ProcessedData/random_coords_insertions.bed -b ../RawData/cg_dmrs.tsv.gz -d -wb \
+    | awk 'BEGIN {FS=OFS="\t"} {if ($7 < 1000) print $0}' - \
+    | wc -l \
+    >> ../ProcessedData/random_selections_cg_dmr_rep_10k_insertions.txt
+
+    ### deletions
+    # choose random coordinates
+    python choose_random_coords.py $deletions $del_size \
+    | sort -k1,1 -k2,2n - \
+    > ../ProcessedData/random_coords_deletions.bed
+
+    # c-dmrs
+    bedtools closest -a ../ProcessedData/random_coords_deletions.bed -b ../RawData/c_dmrs.tsv.gz -d -wb \
+    | awk 'BEGIN {FS=OFS="\t"} {if ($7 < 1000) print $0}' - \
+    | wc -l \
+    >> ../ProcessedData/random_selections_c_dmr_rep_10k_deletions.txt
+
+    #cg-dmrs
+    bedtools closest -a ../ProcessedData/random_coords_deletions.bed -b ../RawData/cg_dmrs.tsv.gz -d -wb \
+    | awk 'BEGIN {FS=OFS="\t"} {if ($7 < 1000) print $0}' - \
+    | wc -l \
+    >> ../ProcessedData/random_selections_cg_dmr_rep_10k_deletions.txt
+
+    ### intermediate frequency
+    # choose random coordinates
+    python choose_random_coords.py $inter $inter_size \
+    | sort -k1,1 -k2,2n - \
+    > ../ProcessedData/random_coords_intermediate.bed
+
+    # c-dmrs
+    bedtools closest -a ../ProcessedData/random_coords_intermediate.bed -b ../RawData/c_dmrs.tsv.gz -d -wb \
+    | awk 'BEGIN {FS=OFS="\t"} {if ($7 < 1000) print $0}' - \
+    | wc -l \
+    >> ../ProcessedData/random_selections_c_dmr_rep_10k_intermediate.txt
+
+    #cg-dmrs
+    bedtools closest -a ../ProcessedData/random_coords_intermediate.bed -b ../RawData/cg_dmrs.tsv.gz -d -wb \
+    | awk 'BEGIN {FS=OFS="\t"} {if ($7 < 1000) print $0}' - \
+    | wc -l \
+    >> ../ProcessedData/random_selections_cg_dmr_rep_10k_intermediate.txt
 done
 
 bedtools closest -a ../ProcessedData/random_coords.bed -b ../RawData/c_dmrs.tsv.gz -d -wb \
@@ -28,3 +100,27 @@ bedtools closest -a ../ProcessedData/random_coords.bed -b ../RawData/c_dmrs.tsv.
 bedtools closest -a ../ProcessedData/random_coords.bed -b ../RawData/cg_dmrs.tsv.gz -d -wb \
 | gzip - \
 > ../ProcessedData/random_coords_closest_cg_dmr.bed.gz
+
+bedtools closest -a ../ProcessedData/random_coords_insertions.bed -b ../RawData/c_dmrs.tsv.gz -d -wb \
+| gzip - \
+> ../ProcessedData/random_coords_closest_c_dmr_insertions.bed.gz
+
+bedtools closest -a ../ProcessedData/random_coords_insertions.bed -b ../RawData/cg_dmrs.tsv.gz -d -wb \
+| gzip - \
+> ../ProcessedData/random_coords_closest_cg_dmr_insertions.bed.gz
+
+bedtools closest -a ../ProcessedData/random_coords_deletions.bed -b ../RawData/c_dmrs.tsv.gz -d -wb \
+| gzip - \
+> ../ProcessedData/random_coords_closest_c_dmr_deletions.bed.gz
+
+bedtools closest -a ../ProcessedData/random_coords_deletions.bed -b ../RawData/cg_dmrs.tsv.gz -d -wb \
+| gzip - \
+> ../ProcessedData/random_coords_closest_cg_dmr_deletions.bed.gz
+
+bedtools closest -a ../ProcessedData/random_coords_intermediate.bed -b ../RawData/c_dmrs.tsv.gz -d -wb \
+| gzip - \
+> ../ProcessedData/random_coords_closest_c_dmr_intermediate.bed.gz
+
+bedtools closest -a ../ProcessedData/random_coords_intermediate.bed -b ../RawData/cg_dmrs.tsv.gz -d -wb \
+| gzip - \
+> ../ProcessedData/random_coords_closest_cg_dmr_intermediate.bed.gz
