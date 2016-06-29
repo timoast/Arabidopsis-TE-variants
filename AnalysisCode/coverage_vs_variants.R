6b7a25dd48639d4ab395b87ad66c09060e36d7c0
timoast <timstuart90@gmail.com>
2016-06-29 09:56:11 +0800

    Add histogram code

--- AnalysisCode/coverage_vs_variants.R
+++ AnalysisCode/coverage_vs_variants.R
@@ -5,9 +5,12 @@ library(tidyr)
 cov <- read_tsv("../ProcessedData/coverages.tsv", col_names = c("Accession", "Coverage"))
 ins <- read_tsv("../ProcessedData/insertion_counts.tsv", col_names = c("Accession", "Insertions"))
 del <- read_tsv("../ProcessedData/deletion_counts.tsv", col_names = c("Accession", "Absences"))
+refine <- read_tsv("../ProcessedData/refinement_stats.tsv", col_names = c("Accession", "Refined"))
 
 dat <- left_join(cov, ins) %>% left_join(del) %>%
   gather(indel, Count, Insertions, Absences)
+
+ref <- left_join(refine, ins) %>% mutate(perc = Refined / Insertions * 100)
 
 model.del <- lm(Count ~ Coverage, data=filter(dat, indel == "Absences"))
 rsq.del <- signif(summary(model.del)$adj.r.squared, 3)
@@ -26,4 +29,8 @@ plot(Count ~ Coverage, data=filter(dat, indel == "Insertions"), ylim=c(0, 800), 
 abline(model.ins)
 title("TE insertions")
 legend("top", paste("Rsq = ", rsq.ins), bty = "n")
+dev.off()
+
+pdf("../Plots/refinement_stats.pdf", height = 4, width = 4)
+hist(ref$perc, breaks = 30, col="blue", main = "TE calls due to TEPID refinement step", xlab = "Percentage of additional TE insertion calls")
 dev.off()
