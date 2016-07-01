aa195e614112f5763b38c892b55403abf16937bb
timoast <timstuart90@gmail.com>
2016-07-01 17:53:37 +0800

    Add RColorbrewer import

--- AnalysisCode/TE_DMR_analysis.R
+++ AnalysisCode/TE_DMR_analysis.R
@@ -2,6 +2,7 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(RColorBrewer)
 
 te_c_dmr <- read_tsv("../ProcessedData/TE_C_DMR_distances.bed.gz", col_names = F) %>%
   rename(dmr_chr = X1, dmr_start = X2, dmr_stop = X3,
@@ -22,8 +23,12 @@ cg <- "#B4B464"
 chg <- "#6665AD"
 chh <- "#B29492"
 
+rare_col <- brewer.pal(6, "Set3")[4]
+common_col <- brewer.pal(6, "Set3")[6]
+
 insertion_col <- brewer.pal(3, "Set2")[1]
 deletion_col <- brewer.pal(3, "Set2")[3]
+na_col <- brewer.pal(9, "Blues")[2]
 
 pdf("../Plots/distances_TEPAV_DMR.pdf", height = 3, width = 3)
 hist(te_c_dmr$distance / 1000,
@@ -39,7 +44,8 @@ te_cg_dmr$AbsenceClassification <- factor(te_cg_dmr$AbsenceClassification, level
 
 # C-DMRs
 ggplot(te_c_dmr, aes(distance / 1000, fill = FrequencyClassification)) +
-  geom_density(alpha = 0.5) + facet_wrap(~FrequencyClassification) + xlim(0, 5) +
+  geom_density(alpha = 0.8) + facet_wrap(~FrequencyClassification) + xlim(0, 5) +
+  scale_fill_manual(values = c(rare_col, common_col)) +
   theme_bw() + xlab("Distance (kb)") + ylab("Density") +
   theme(legend.position="none") +	
   ggsave("../Plots/CDMR/c_dmr_distances_common_vs_rare.pdf", width = 10, height = 6, units = "cm", useDingbats=F)	
@@ -53,9 +59,10 @@ ggplot(na.omit(te_c_dmr), aes(distance / 1000, fill = AbsenceClassification)) +
 
 # CG-DMRs
 ggplot(te_cg_dmr, aes(distance / 1000, fill = FrequencyClassification)) +
-  geom_density(alpha = 0.5) + facet_wrap(~FrequencyClassification) + xlim(0, 5) +
+  geom_density(alpha = 0.8) + facet_wrap(~FrequencyClassification) + xlim(0, 5) +
   theme_bw() + xlab("Distance (kb)") + ylab("Density") +
   theme(legend.position="none") +	
+  scale_fill_manual(values = c(rare_col, common_col)) +
   ggsave("../Plots/CGDMR/cg_dmr_distances_common_vs_rare.pdf", width = 10, height = 6, units = "cm", useDingbats=F)	
 
 ggplot(na.omit(te_cg_dmr), aes(distance / 1000, fill = AbsenceClassification)) +
