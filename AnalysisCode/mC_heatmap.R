264593fed5f3514bb29f94db0d93be40d99db98a
timoast <timstuart90@gmail.com>
2016-07-07 14:48:57 +0800

    Add mC-TEPAV correlation data

--- AnalysisCode/mC_heatmap.R
+++ AnalysisCode/mC_heatmap.R
@@ -116,3 +116,30 @@ line_plot(ins_means_cg_absent, ins_means_chg_absent, ins_means_chh_absent, 0.6)
 line_plot(del_means_cg,del_means_chg,del_means_chh, 0.6)
 line_plot(del_means_cg_absent, del_means_chg_absent, del_means_chh_absent, 0.6)
 dev.off()
+
+# correlation between flanking DNA methylation levels and TE presence/absence
+flanking_mc_corr <- read_tsv("../ProcessedData/DNAmethylation/flanking_mc_correlations.txt") %>%
+  mutate(id = paste(chr, start, stop)) %>%
+  select(-(chr:stop))
+
+tepav <- read_tsv("../RawData/TEPID_TEPAV.tsv.gz") %>%
+  mutate(id = paste(chromosome, start, end))
+
+data <- left_join(tepav, flanking_mc_corr, by="id") %>%
+  select(-id)
+
+data$LD <- factor(data$LD, levels = c("high", "mid", "low", NA))
+
+ggplot(data, aes(Absence_classification, cor, fill=LD)) +
+  geom_boxplot(outlier.shape = 1) + theme_bw() +
+  scale_fill_brewer(type="seq", palette = 11, direction = -1) +
+  geom_hline(yintercept = 0) +
+  coord_flip() + ylab("Correlation") + xlab("TE variant") +
+  ggsave("../Plots/mc_flanking_correlation.pdf", height = 8, width = 15, units = "cm")
+
+ggplot(data, aes(MAF, cor, fill=LD)) +
+  geom_boxplot(aes(group = cut_width(MAF, 0.04)), outlier.shape = 1, outlier.size = 0.8) +
+  theme_bw() + geom_hline(yintercept = 0) + facet_wrap(~LD, ncol = 1) +
+  scale_fill_brewer(type="seq", palette = 11, direction = -1) +
+  theme(text = element_text(size=8)) +
+  ggsave("../Plots/mc_flanking_correlation_by_ld.pdf", height = 20, width = 8, units = "cm", useDingbats=F)
