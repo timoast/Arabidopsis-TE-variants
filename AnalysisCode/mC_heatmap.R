4a63e3b1f3307fe3a890767d76edc9d58a38d9c8
timoast <timstuart90@gmail.com>
2016-07-13 18:30:02 +0800

    Separate by distance to centromere

--- AnalysisCode/mC_heatmap.R
+++ AnalysisCode/mC_heatmap.R
@@ -3,38 +3,83 @@ library(RColorBrewer)
 library(readr)
 library(ggplot2)
 
-mc_no_insertion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_allC_no_insertion.tsv.gz", col_names = T, na = c("NaN"))
-mc_true_deletion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_allC_true_deletions.tsv.gz", col_names = T, na = c("NaN"))
+mc_no_insertion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_allC_no_insertion.tsv.gz",
+                                col_names = T, na = c("nan")) %>%
+  mutate(class = "Insertion")
+
+mc_true_deletion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_allC_true_deletions.tsv.gz",
+                                 col_names = T, na = c("nan")) %>%
+  mutate(class = "Deletion")
+
+# mc_all <- read_tsv("../ProcessedData/DNAmethylation/flanking_allC.tsv.gz", col_names = T, na = c("nan"))
+
+all <- rbind(mc_true_deletion_2kb, mc_no_insertion_2kb)
+
+# Add column with distance to centromere
+get_centro_distance <- function(crds, centromeres) {
+  crds <- unlist(strsplit(crds, ","))
+  cent <- filter(centromeres, chrom == crds[[1]])
+  midpoint <- (cent[[3]] + cent[[2]]) / 2
+  d <- abs(as.numeric(crds[[2]]) - midpoint)
+  return(d)
+}
+
+centromeres <- read_tsv("../RawData/centromere_positions.txt", col_names = c("chrom", "start", "stop"))
+
+# separate insertions into pericentromeric / euchromatic
+# sort within by mC
+# make line charts separately for each
 
 # sum rows for sort order
-mc_no_insertion_2kb.sums <- mutate(mc_no_insertion_2kb, sums=rowSums(mc_no_insertion_2kb[,2:41], na.rm = T))
-mc_true_deletion_2kb.sums <- mutate(mc_true_deletion_2kb, sums=rowSums(mc_true_deletion_2kb[,2:41], na.rm = T))
+all.sort <- all %>%
+  mutate(mn = rowMeans(all[,2:41], na.rm = T)) %>%
+  rowwise() %>%
+  mutate(distance = get_centro_distance(coords, centromeres),
+         pericentromeric = distance < 3 * 10^6) %>%
+  arrange(class, pericentromeric, mn)
 
-# Sort by total mC level
-mc_no_insertion_2kb.sort <- arrange(mc_no_insertion_2kb.sums, sums) %>% select(-sums)
-mc_true_deletion_2kb.sort <- arrange(mc_true_deletion_2kb.sums, sums) %>% select(-sums)
+dels <- filter(all.sort, class == "Deletion")
+ins <- filter(all.sort, class == "Insertion")
+
+distance_dels <- select(dels, distance)
+distance_ins <- select(ins, distance)
 
 scale_max <- function(m, l){
   m.floor <- m > l
   m[m.floor] <- l
   return(t(data.matrix(m)))
 }
-both <- rbind(mc_true_deletion_2kb.sort, mc_no_insertion_2kb.sort)
-ins <- scale_max(mc_no_insertion_2kb.sort[,2:40], 0.5)
-del <- scale_max(mc_true_deletion_2kb.sort[,2:40], 0.5)
-both_scaled <- scale_max(both[,2:40], 0.5)
+
+
+both_scaled <- scale_max(all.sort[,2:40], 0.5)
+ins_scaled <- scale_max(ins[,2:40], 0.5)
+del_scaled <- scale_max(dels[,2:40], 0.5)
+
 color <- colorRampPalette(brewer.pal(9,"Reds"))(100)
+d_color <- colorRampPalette(brewer.pal(9, "YlOrBr"))(100)
+
+png("../Plots/heatmap_mc.png", height = 6, width = 3, units = "in", res = 600, bg = "grey")
+image(both_scaled, col = color)
+dev.off()
 
 png("../Plots/heatmap_insertion.png", height = 6, width = 3, units = "in", res = 600, bg = "grey")
-image(ins, col = color, main = "TE insertions")
+image(ins_scaled, col = color, main = "TE insertions")
 dev.off()
 
 png("../Plots/heatmap_deletion.png", height = 6, width = 3, units = "in", res = 600, bg = "grey")
-image(del, col = color, main = "TE deletions")
+image(del_scaled, col = color, main = "TE deletions")
 dev.off()
 
 png("../Plots/heatmap_ins_del.png", height = 6, width = 3, units = "in", res = 600, bg = "grey")
 image(both_scaled, col=color)
+dev.off()
+
+png("../Plots/heatmap_centromere_distances_deletions.png", height = 6, width = 3, units = "in", res = 600)
+image(scale_max(distance_dels, 15 * 10^6), col = d_color)
+dev.off()
+
+png("../Plots/heatmap_centromere_distances_insertion.png", height = 6, width = 3, units = "in", res = 600)
+image(scale_max(distance_ins, 15 * 10^6), col = d_color)
 dev.off()
 
 # scale bar
@@ -42,35 +87,78 @@ pdf("../Plots/mc_heatmap_scale.pdf", height=2, width=5)
 image(data.matrix(seq(100)), col = color)
 dev.off()
 
+pdf("../Plots/scale_ylorbr.pdf", height=2, width=5)
+image(data.matrix(seq(100)), col = d_color)
+dev.off()
+
 # make line charts of each mC context
+# Load context-specific methylation data
+# join with pericentromeric T/F and methylation sum column from allC context data and sort to get same sort order
+# Separate by pericentromeric T/F and plot line chart of column means
 
-mCG_no_insertion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CG_no_insertion.tsv.gz", col_names = T)
-mCG_true_deletion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CG_true_deletions.tsv.gz", col_names = T)
+ins_data <- select(ins, coords, mn, pericentromeric)
+del_data <- select(dels, coords, mn, pericentromeric)
 
-mCHG_no_insertion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CHG_no_insertion.tsv.gz", col_names = T)
-mCHG_true_deletion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CHG_true_deletions.tsv.gz", col_names = T)
+mCG_no_insertion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CG_no_insertion.tsv.gz", col_names = T, na = c("nan")) %>%
+  left_join(., ins_data, by="coords") %>%
+  arrange(pericentromeric, mn)
 
-mCHH_no_insertion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CHH_no_insertion.tsv.gz", col_names = T)
-mCHH_true_deletion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CHH_true_deletions.tsv.gz", col_names = T)
+mCG_true_deletion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CG_true_deletions.tsv.gz", col_names = T, na = c("nan")) %>%
+  left_join(., del_data, by="coords") %>%
+  arrange(pericentromeric, mn)
+
+mCHG_no_insertion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CHG_no_insertion.tsv.gz", col_names = T, na = c("nan")) %>%
+  left_join(., ins_data, by="coords") %>%
+  arrange(pericentromeric, mn)
+
+mCHG_true_deletion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CHG_true_deletions.tsv.gz", col_names = T, na = c("nan")) %>%
+  left_join(., del_data, by="coords") %>%
+  arrange(pericentromeric, mn)
+
+mCHH_no_insertion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CHH_no_insertion.tsv.gz", col_names = T, na = c("nan")) %>%
+  left_join(., ins_data, by="coords") %>%
+  arrange(pericentromeric, mn)
+
+mCHH_true_deletion_2kb <- read_tsv("../ProcessedData/DNAmethylation/flanking_CHH_true_deletions.tsv.gz", col_names = T, na = c("nan")) %>%
+  left_join(., del_data, by="coords") %>%
+  arrange(pericentromeric, mn)
 
 # get colMeans
-ins_means_cg <- colMeans(mCG_no_insertion_2kb[,2:21], na.rm = T)
-ins_means_cg_absent <- colMeans(mCG_no_insertion_2kb[,21:41], na.rm = T)
+ins_means_cg_eu <- colMeans(filter(mCG_no_insertion_2kb, pericentromeric == F)[,2:21], na.rm = T)
+ins_means_cg_het <- colMeans(filter(mCG_no_insertion_2kb, pericentromeric == T)[,2:21], na.rm = T)
 
-ins_means_chg <- colMeans(mCHG_no_insertion_2kb[,2:21], na.rm = T)
-ins_means_chg_absent <- colMeans(mCHG_no_insertion_2kb[,21:41], na.rm = T)
+ins_means_cg_absent_eu <- colMeans(filter(mCG_no_insertion_2kb, pericentromeric == F)[,21:41], na.rm = T)
+ins_means_cg_absent_het <- colMeans(filter(mCG_no_insertion_2kb, pericentromeric == T)[,21:41], na.rm = T)
 
-ins_means_chh <- colMeans(mCHH_no_insertion_2kb[,2:21], na.rm = T)
-ins_means_chh_absent <- colMeans(mCHH_no_insertion_2kb[,21:41], na.rm = T)
+ins_means_chg_eu <- colMeans(filter(mCHG_no_insertion_2kb, pericentromeric == F)[,2:21], na.rm = T)
+ins_means_chg_het <- colMeans(filter(mCHG_no_insertion_2kb, pericentromeric == T)[,2:21], na.rm = T)
 
-del_means_cg <- colMeans(mCG_true_deletion_2kb[,2:21], na.rm = T)
-del_means_cg_absent <- colMeans(mCG_true_deletion_2kb[,21:41], na.rm = T)
+ins_means_chg_absent_eu <- colMeans(filter(mCHG_no_insertion_2kb, pericentromeric == F)[,21:41], na.rm = T)
+ins_means_chg_absent_het <- colMeans(filter(mCHG_no_insertion_2kb, pericentromeric == T)[,21:41], na.rm = T)
 
-del_means_chg <- colMeans(mCHG_true_deletion_2kb[,2:21], na.rm = T)
-del_means_chg_absent <- colMeans(mCHG_true_deletion_2kb[,21:41], na.rm = T)
+ins_means_chh_eu <- colMeans(filter(mCHH_no_insertion_2kb, pericentromeric == F)[,2:21], na.rm = T)
+ins_means_chh_het <- colMeans(filter(mCHH_no_insertion_2kb, pericentromeric == T)[,2:21], na.rm = T)
 
-del_means_chh <- colMeans(mCHH_true_deletion_2kb[,2:21], na.rm = T)
-del_means_chh_absent <- colMeans(mCHH_true_deletion_2kb[,21:41], na.rm = T)
+ins_means_chh_absent_eu <- colMeans(filter(mCHH_no_insertion_2kb, pericentromeric == F)[,21:41], na.rm = T)
+ins_means_chh_absent_het <- colMeans(filter(mCHH_no_insertion_2kb, pericentromeric == T)[,21:41], na.rm = T)
+
+del_means_cg_eu <- colMeans(filter(mCG_true_deletion_2kb, pericentromeric == F)[,2:21], na.rm = T)
+del_means_cg_het <- colMeans(filter(mCG_true_deletion_2kb, pericentromeric == T)[,2:21], na.rm = T)
+
+del_means_cg_absent_eu <- colMeans(filter(mCG_true_deletion_2kb, pericentromeric == F)[,21:41], na.rm = T)
+del_means_cg_absent_het <- colMeans(filter(mCG_true_deletion_2kb, pericentromeric == T)[,21:41], na.rm = T)
+
+del_means_chg_eu <- colMeans(filter(mCHG_true_deletion_2kb, pericentromeric == F)[,2:21], na.rm = T)
+del_means_chg_het <- colMeans(filter(mCHG_true_deletion_2kb, pericentromeric == T)[,2:21], na.rm = T)
+
+del_means_chg_absent_eu <- colMeans(filter(mCHG_true_deletion_2kb, pericentromeric == F)[,21:41], na.rm = T)
+del_means_chg_absent_het <- colMeans(filter(mCHG_true_deletion_2kb, pericentromeric == T)[,21:41], na.rm = T)
+
+del_means_chh_eu <- colMeans(filter(mCHH_true_deletion_2kb, pericentromeric == F)[,2:21], na.rm = T)
+del_means_chh_het <- colMeans(filter(mCHH_true_deletion_2kb, pericentromeric == T)[,2:21], na.rm = T)
+
+del_means_chh_absent_eu <- colMeans(filter(mCHH_true_deletion_2kb, pericentromeric == F)[,21:41], na.rm = T)
+del_means_chh_absent_het <- colMeans(filter(mCHH_true_deletion_2kb, pericentromeric == T)[,21:41], na.rm = T)
 
 # define colours
 cg <- '#B4B464'
@@ -101,12 +189,19 @@ line_plot <- function(d1, d2, d3, scale) {
   )
 }
 
-pdf("../Plots/flanking_mc_line_charts.pdf", height=4, width=4, useDingbats = F)
-par(mfrow=c(2,2), mar=c(2,2,2,2))
-line_plot(ins_means_cg,ins_means_chg,ins_means_chh, 0.8)
-line_plot(ins_means_cg_absent, ins_means_chg_absent, ins_means_chh_absent, 0.8)
-line_plot(del_means_cg,del_means_chg,del_means_chh, 0.8)
-line_plot(del_means_cg_absent, del_means_chg_absent, del_means_chh_absent, 0.8)
+pdf("../Plots/flanking_mc_line_charts.pdf", height=5, width=3, useDingbats = F)
+par(mfrow=c(4, 2), mar=c(2,2,2,2), cex = 0.8)
+line_plot(ins_means_cg_het,ins_means_chg_het,ins_means_chh_het, 1)
+line_plot(ins_means_cg_absent_het,ins_means_chg_absent_het,ins_means_chh_absent_het, 1)
+
+line_plot(ins_means_cg_eu,ins_means_chg_eu,ins_means_chh_eu, 0.5)
+line_plot(ins_means_cg_absent_eu, ins_means_chg_absent_eu, ins_means_chh_absent_eu, 0.5)
+
+line_plot(del_means_cg_het, del_means_chg_het, del_means_chh_het , 1)
+line_plot(del_means_cg_absent_het, del_means_chg_absent_het, del_means_chh_absent_het, 1)
+
+line_plot(del_means_cg_eu, del_means_chg_eu, del_means_chh_eu , 0.5)
+line_plot(del_means_cg_absent_eu, del_means_chg_absent_eu, del_means_chh_absent_eu, 0.5)
 dev.off()
 
 # Bud vs lead methylation
