b14c4e98dada508e2875651d5383c850da756a43
timoast <timstuart90@gmail.com>
2016-07-27 12:07:28 +0800

    Added histograms showing association counts for TEs

--- AnalysisCode/plot_trans_scatter.R
+++ AnalysisCode/plot_trans_scatter.R
@@ -47,3 +47,32 @@ pvals %>%
         panel.margin = unit(0, "lines"))
 
 ggsave("../Plots/pval_ins.png", height=5, width = 5, dpi = 600)
+
+dot_plot <- function(pval, chr, y) {
+  filtered <- subset(pval, te_chr == chr)
+  h <- hist(filtered$te_start, breaks = 10000, plot=F)
+  d <- data.frame(x = h$breaks[1:length(h$breaks) - 1], y = h$counts)
+  d <- d[d$y > 0,]
+  plot(d, pch=19, cex=0.5, ylim = c(0, y))
+}
+
+ins <- filter(pvals, indel == "Insertion")
+del <- filter(pvals, indel == "Deletion")
+
+pdf("../Plots/trans_dotplot_insertions.pdf", height=1.5, width=10, useDingbats = F)
+par(mfrow=c(1,5), mar = c(1,1,1,1))
+dot_plot(ins, "chr1", 400)
+dot_plot(ins, "chr2", 400)
+dot_plot(ins, "chr3", 400)
+dot_plot(ins, "chr4", 400)
+dot_plot(ins, "chr5", 400)
+dev.off()
+
+pdf("../Plots/trans_dotplot_deletions.pdf", height=1.5, width=10, useDingbats = F)
+par(mfrow=c(1,5), mar = c(1,1,1,1))
+dot_plot(del, "chr1", 700)
+dot_plot(del, "chr2", 700)
+dot_plot(del, "chr3", 700)
+dot_plot(del, "chr4", 700)
+dot_plot(del, "chr5", 700)
+dev.off()
