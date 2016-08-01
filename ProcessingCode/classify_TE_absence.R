5b86446758c59c1e1d66aecc1531d856023cdb70
timoast <timstuart90@gmail.com>
2016-08-01 11:35:23 +0800

    Add code and plots showing number accessions sharing variants

--- ProcessingCode/classify_TE_absence.R
+++ ProcessingCode/classify_TE_absence.R
@@ -14,13 +14,38 @@ polarized <- poly %>%
          } else {
            "NA"
          },
+         original_call_deletion = grepl("Col-0", X5),
          MAF = min(TE_present, TE_absent) / 216,
          Frequency_classification = ifelse(MAF < 0.03, "Rare", "Common")) %>%
   ungroup()
 
+ggplot(filter(polarized, original_call_deletion == FALSE), aes(TE_present)) +
+  geom_histogram(bins = 50, color="black") + theme_bw() + ggtitle("TE insertion calls") +
+  xlab("Accessions sharing TE insertion")
+ggsave("../Plots/accessions_sharing_te_insertion.pdf", height = 3, width = 5)
+
+# polarized %>%
+#   filter(original_call_deletion == FALSE) %>%
+#   group_by(TE_present > 1) %>%
+#   count()
+
+ggplot(filter(polarized, original_call_deletion == TRUE), aes(TE_absent)) +
+  geom_histogram(bins = 50, color="black") + theme_bw() + ggtitle("TE absence calls") +
+  xlab("Accessions sharing TE absence")
+ggsave("../Plots/accessions_sharing_te_absence.pdf", height = 3, width = 5)
+
+# polarized %>%
+#   filter(original_call_deletion == TRUE) %>%
+#   group_by(TE_absent > 1) %>%
+#   count()
+
+polarized <- select(polarized, -original_call_deletion)
+
 colnames(polarized) <- c("chromosome", "start", "end", "TE", "Accessions_TE_present", "Accessions_TE_absent", "Count_TE_present", "Count_TE_absent", "Minor_allele", "Absence_classification", "MAF", "Frequency_classification")
 # add the LD data
 polarized <- mutate(polarized, id = paste(chromosome, start, TE))
+
+keys <- list(old = "high", mid = "mid", young = "low")
 
 ld <- read_tsv("../RawData/snp_association.tsv.gz") %>%
   rowwise() %>%
